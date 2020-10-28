8eb2b6426d45df0db2d3bf108bef4e3752aaf704
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2020-10-28 14:21:22 +0800

    fix: add fail.ts errorMessage to toTestResult options.tests

--- lib/fail.ts
+++ lib/fail.ts
@@ -1,20 +1,35 @@
+import type { TestResult } from '@jest/test-result';
 import toTestResult from './toTestResult';
 import type { Path } from './types';
 
 interface Options {
   start: number;
   end: number;
-  test: {
-    title: string;
-    path: Path;
-    errorMessage?: string;
-  };
+  test: { title: string; path: Path; errorMessage?: string };
   errorMessage?: string;
 }
 
-const fail = ({ start, end, test, errorMessage }: Options) =>
-  toTestResult({
-    errorMessage: errorMessage || test.errorMessage,
+export default function fail(options: {
+  start: number;
+  end: number;
+  test: { title: string; path: Path; errorMessage: string };
+}): TestResult;
+
+export default function fail(options: {
+  start: number;
+  end: number;
+  test: { title: string; path: Path };
+  errorMessage: string;
+}): TestResult;
+
+export default function fail({ start, end, test, errorMessage }: Options) {
+  // TODO: Currently the `fail` function allows 2 ways to pass an error message.
+  // Both methods are currently in used by downstream packages.
+  // The current behaviour is to favour `errorMessage` over `test.errorMessage`.
+  const actualErrorMessage = errorMessage || test.errorMessage;
+
+  return toTestResult({
+    errorMessage: actualErrorMessage,
     stats: {
       failures: 1,
       pending: 0,
@@ -24,8 +39,9 @@ const fail = ({ start, end, test, errorMessage }: Options) =>
       end,
     },
     skipped: false,
-    tests: [{ duration: end - start, ...test }],
+    tests: [
+      { duration: end - start, ...test, errorMessage: actualErrorMessage },
+    ],
     jestTestPath: test.path,
   });
-
-export default fail;
+}
