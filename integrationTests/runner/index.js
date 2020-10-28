3956e25829033e13cdb36d0e26a61d9f8fd38014
LS Hung <61226932+lokshunhung@users.noreply.github.com>
2020-10-28 16:51:34 +0800

    chore: disable import/no-unresolved lint for build artifacts

--- integrationTests/runner/index.js
+++ integrationTests/runner/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-unresolved -- ignore unresolved build artifact
 const { createJestRunner } = require('../..');
 
 module.exports = createJestRunner(require.resolve('./run'));
